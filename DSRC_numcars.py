685b9f7fc00e01016f6b0ebb680aab33d1b910af
AineeJames <olsenaiden33@gmail.com>
2023-03-11 20:11:40 -0800

    added input for tx option

--- DSRC_numcars.py
+++ DSRC_numcars.py
@@ -12,11 +12,15 @@ parser.add_argument("--process-only", action="store_true", help="pass this flag 
 parser.add_argument("--mode", choices=['cars', 'txpwr'], help="chooses what varible to change each time a test is ran")
 args = parser.parse_args()
 
-def run_sim(num_cars, queue, SIM_TIME):
-    os.system(f"../ns3 run 'vanet-routing-compare --totaltime={SIM_TIME} --nodes={num_cars} --CSVfileName=ns-3_script/pdrVScars/{num_cars}cars.csv --CSVfileName=ns-3_script/pdrVScars/{num_cars}cars_summary.csv' > /dev/null 2>&1")
-    queue.put(num_cars)
+def run_sim(step, queue, SIM_TIME):
+    if args.mode == 'cars':
+        os.system(f"../ns3 run 'vanet-routing-compare --totaltime={SIM_TIME} --nodes={step} --CSVfileName=ns-3_script/pdrVScars/{step}cars.csv --CSVfileName=ns-3_script/pdrVScars/{step}cars_summary.csv' > /dev/null 2>&1")
+    elif args.mode == 'txpwr':
+        #TODO CHANGE THIS LINE os.system(f"../ns3 run 'vanet-routing-compare --totaltime={SIM_TIME} --nodes={step} --CSVfileName=ns-3_script/pdrVScars/{step}cars.csv --CSVfileName=ns-3_script/pdrVScars/{step}cars_summary.csv' > /dev/null 2>&1")
+    queue.put(step)
 
 if __name__ == "__main__":
+
     if not os.path.exists("pdrVScars"):
         print("Creating pdrVScars directory...")
         os.mkdir("pdrVScars")
@@ -32,17 +36,30 @@ if __name__ == "__main__":
             print("Changing the tx power.")
 
         print("What settings do you wish to use?")
-        print("Please enter settings with the folowing format: sim_time min_cars max_cars step")
-        settings_input = input("Settings: ")
-        settings = settings_input.split(' ')
-        if len(settings) != 4:
-            print("Not enough arguments provided, exiting...")
-            exit()
-        SIM_TIME = int(settings[0])
-        MIN_CARS = int(settings[1])
-        MAX_CARS = int(settings[2])
-        STEP = int(settings[3])
-        cars = list(range(MIN_CARS, MAX_CARS + 1, STEP))
+        if args.mode == 'cars':
+            print("Please enter settings with the folowing format: sim_time min_cars max_cars step")
+            settings_input = input("Settings: ")
+            settings = settings_input.split(' ')
+            if len(settings) != 4:
+                print("Not enough arguments provided, exiting...")
+                exit()
+            SIM_TIME = int(settings[0])
+            MIN_CARS = int(settings[1])
+            MAX_CARS = int(settings[2])
+            STEP = int(settings[3])
+            cars = list(range(MIN_CARS, MAX_CARS + 1, STEP))
+        elif args.mode == 'txpwr':
+            print("Please enter settings with the folowing format: sim_time min_txpwr max_txpwr step")
+            settings_input = input("Settings: ")
+            settings = settings_input.split(' ')
+            if len(settings) != 4:
+                print("Not enough arguments provided, exiting...")
+                exit()
+            SIM_TIME = int(settings[0])
+            MIN_TXPWR = int(settings[1])
+            MAX_TXPWR = int(settings[2])
+            STEP = int(settings[3])
+            tx = list(range(MIN_TXPWR, MAX_TXPWR + 1, STEP))
 
         input = input("This action will remove previous data, do you want to proceed (y/n)? ")
         if input.lower() != "y":
@@ -57,24 +74,32 @@ if __name__ == "__main__":
             if os.path.isfile(file_path):
                 os.remove(file_path)
 
-        print("============================================================================")
-        print(f"Running a total of {len(cars)} simulations w/ a simulation time of {SIM_TIME}")
-        print(f"Starting car number is {MIN_CARS}")
-        print(f"Ending car number is {MAX_CARS}")
-        print(f"Stepping by {STEP} cars")
-        print("============================================================================")
+        if args.mode == 'cars':
+            print("============================================================================")
+            print(f"Running a total of {len(cars)} simulations w/ a simulation time of {SIM_TIME}")
+            print(f"Starting car number is {MIN_CARS}")
+            print(f"Ending car number is {MAX_CARS}")
+            print(f"Stepping by {STEP}")
+            print("============================================================================")
+        elif args.mode == 'txpwr':
+            print("============================================================================")
+            print(f"Running a total of {len(tx)} simulations w/ a simulation time of {SIM_TIME}")
+            print(f"Starting tx power is {MIN_TXPWR}")
+            print(f"Ending tx power is {MAX_TXPWR}")
+            print(f"Stepping by {STEP}")
+            print("============================================================================")
 
         queue = multiprocessing.Queue()
         processes = []
-        for num_cars in cars:
-            process = multiprocessing.Process(target=run_sim, args=(num_cars, queue, SIM_TIME))
+        for step in (cars if args.mode == 'cars' else tx):
+            process = multiprocessing.Process(target=run_sim, args=(step, queue, SIM_TIME))
             process.start()
             processes.append(process)
 
         # Update progress bar
         for i in tqdm(range(len(cars)), ascii=False):
             process_num = queue.get()
-            tqdm.write(f"Thread w/ {process_num} cars just finished...")
+            tqdm.write(f"Thread w/ {process_num} {'cars' if args.mode == 'cars' else 'txpwr'} just finished...")
 
         # Wait for all processs to finish
         for process in processes:
